84133c23a9458ba934f7c9d288deb5dce14b5b6c
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-06 15:03:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,7 +14,7 @@ while row_index < src.count do
  
     
     
-    if src[row_index][element_index].length < smallest_number.length 
+    if src[row_index][element_index] < smallest_number 
       smallest_number = src[row_index][element_index]
     end
     element_index += 1
